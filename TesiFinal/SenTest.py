53ed93f520a83b2d7b4ef3c83b6cb5132d2eccd0
lucamuggiasca <lucamuggiasca@outlook.com>
2023-11-08 15:40:03 +0100

    Test for sentiment

---
+++ TesiFinal/SenTest.py
@@ -0,0 +1,24 @@
+import yfinance as yf
+import matplotlib.pyplot as plt
+import Sentiment
+import numpy as np
+
+# Carica i dati tramite api yfinance
+TITMI = yf.Ticker("TIT.MI")
+data= TITMI.history(period="1Y",actions=False) 
+data.reset_index(inplace=True)
+
+#Sentiment
+print(data.index[0])
+print(data.index[-1])
+SA = Sentiment.Sentiment_Analysis(query="Telecom", language='it' ,country='IT', start=data.index[0], end=data.index[-1])
+polarity_array = np.array('')
+for idx in data.index:
+    print(idx)
+    polarity_array = np.append(polarity_array, SA.do_Analysis(idx, debug=True))
+    
+    
+data['polarity'] = polarity_array[:-1] # probabilmente va anche fatto un flip
+plt.plot(polarity_array)
+plt.show()
+
